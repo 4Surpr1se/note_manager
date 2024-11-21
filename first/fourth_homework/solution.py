0a1fb24132bb86220acdb37af008d06e704b682c
4Surpr1se <99213922+4Surpr1se@users.noreply.github.com>
2024-11-21 22:19:12 +0300

    first_task_update

--- first/fourth_homework/solution.py
+++ first/fourth_homework/solution.py
@@ -1,15 +1,25 @@
-# Пример переменных
-username = "Maxon_Sosison"
-title = "Моя первая заметка"
-content = "Это содержание заметки."
-status = "В работе"
-created_date = "10-11-2024"
-issue_date = "10-12-2024"
+titles = []
+
+username = input("Введите имя пользователя: ")
+content = input("Введите содержание заметки: ")
+status = input("Введите статус заметки (например, 'В работе'): ")
+created_date = input("Введите дату создания заметки (в формате дд-mm-yyyy): ")
+issue_date = input("Введите дату истечения заметки (в формате дд-mm-yyyy): ")
+
+# Запрос заголовков заметок
+title1 = input("Введите заголовок первой заметки: ")
+title2 = input("Введите заголовок второй заметки: ")
+title3 = input("Введите заголовок третьей заметки: ")
+
+# Добавление заголовков в список
+titles.append(title1)
+titles.append(title2)
+titles.append(title3)
 
 # Вывод информации о заметке
 print("Имя пользователя:", username)
-print("Заголовок:", title)
+print("Заголовки:", titles)
 print("Содержание:", content)
 print("Статус:", status)
-print("Дата создания:", created_date[:-5])
-print("Дата истечения:", issue_date[:-5])
+print("Дата создания:", created_date)
+print("Дата истечения:", issue_date)
